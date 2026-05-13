d65e618a72b862169f652b12cf8e0d6ebffe13b5
unknown <nhdphuong1130@gmail.com>
2026-05-13 18:34:02 +0700

    hoan thanh ex04

--- lab01/ex04/QuanLySinhVien.py
+++ lab01/ex04/QuanLySinhVien.py
@@ -1,54 +1,89 @@
 from sinhvien import SinhVien
 
 class QuanLySinhVien:
-    listSinhVien = []
-    
-def generateID(self):
-    maxID = 1
-    if (self.soLuongSinhVien() > 0):
-        maxID = self.listSinhvien[0]._id
+    def __init__(self):
+        self.listSinhVien = []
+
+    def generateID(self):
+        maxID = 1
+        if len(self.listSinhVien) > 0:
+            maxID = self.listSinhVien[0]._id
+            for sv in self.listSinhVien:
+                if maxID < sv._id:
+                    maxID = sv._id
+            maxID += 1
+        return maxID
+
+    def soLuongSinhVien(self):
+        return len(self.listSinhVien)
+
+    def nhapSinhVien(self):
+        id = self.generateID()
+        name = input("Nhap ten sinh vien: ")
+        sex = input("Nhap gioi tinh: ")
+        major = input("Nhap chuyen nganh: ")
+        diemtb = float(input("Nhap diem trung binh: "))
+
+        sv = SinhVien(id, name, sex, major, diemtb)
+        self.xeploaiHocLuc(sv)
+        self.listSinhVien.append(sv)
+
+    def updateSinhVien(self, id):
         for sv in self.listSinhVien:
-            if (maxID < sv._id):
-                maxID = sv._id
-        maxID = maxID + 1
-    return maxID
+            if sv._id == id:
+                sv._name = input("Nhap ten sinh vien: ")
+                sv._sex = input("Nhap gioi tinh: ")
+                sv._major = input("Nhap chuyen nganh: ")
+                sv._diemtb = float(input("Nhap diem trung binh: "))
+                self.xeploaiHocLuc(sv)
+                return True
+        print("Khong tim thay sinh vien")
+        return False
 
-def soLuongSinhVien(self):
-    return len(self.listSinhVien)
+    def deleteById(self, id):
+        for sv in self.listSinhVien:
+            if sv._id == id:
+                self.listSinhVien.remove(sv)
+                return True
+        return False
 
-def NhapSinhVien(self):
-    id = self.generateID()
-    name = input("Nhap ten sinh vien: ")
-    sex = input("Nhap gioi tinh: ")
-    major = input("Nhap chuyen nganh: ")
-    diemtb = float(input("Nhap diem trung binh: "))
-    sv = SinhVien(id, name, sex, major, diemtb)
-    self.xeploaiHocLuc(sv)
-    self.listSinhVien.append(sv)
-    
-def updateSinhVien(self, id):
-    for sv in self.listSinhVien:
-        if (sv._id == id):
-            name = input("Nhap ten sinh vien: ")
-            sex = input("Nhap gioi tinh: ")
-            major = input("Nhap chuyen nganh: ")
-            diemtb = float(input("Nhap diem trung binh: "))
-            sv._name = name
-            sv._sex = sex
-            sv._major = major
-            sv._diemtb = diemtb
-            self.xeploaiHocLuc(sv)
-        else:
-            print("Khong tim thay sinh vien co id: ", id)
-    
-    def sortByID(self):
-        self.listSinhVien.sort(key=lambda sv: sv._id, reverse=False)
-        
-    def sortByName(self):
-        self.listSinhVien.sort(key=lambda sv: sv._name, reverse=False)
-        
+    def findByName(self, keyword):
+        result = []
+        for sv in self.listSinhVien:
+            if keyword.upper() in sv._name.upper():
+                result.append(sv)
+        return result
+
     def sortByDiemTB(self):
-        self.listSinhVien.sort(key=lambda sv: sv._diemtb, reverse=False)
-        
-    def findByID(self, id)
-        
+        self.listSinhVien.sort(key=lambda sv: sv._diemtb)
+
+    def sortByName(self):
+        self.listSinhVien.sort(key=lambda sv: sv._name)
+
+    def xeploaiHocLuc(self, sv):
+        if sv._diemtb >= 8:
+            sv._xeploai = "Gioi"
+        elif sv._diemtb >= 6.5:
+            sv._xeploai = "Kha"
+        elif sv._diemtb >= 5:
+            sv._xeploai = "Trung Binh"
+        else:
+            sv._xeploai = "Yeu"
+
+    def showSinhVien(self, listSV):
+        print("{:<5} {:<20} {:<10} {:<15} {:<10} {:<10}".format(
+            "ID", "Ten", "GioiTinh", "Nganh", "DiemTB", "HocLuc"
+        ))
+
+        for sv in listSV:
+            print("{:<5} {:<20} {:<10} {:<15} {:<10} {:<10}".format(
+                sv._id,
+                sv._name,
+                sv._sex,
+                sv._major,
+                sv._diemtb,
+                sv._xeploai
+            ))
+
+    def getListSinhVien(self):
+        return self.listSinhVien
--- lab01/ex04/sinhvien.py
+++ lab01/ex04/sinhvien.py
@@ -5,4 +5,4 @@ class SinhVien:
         self._sex = sex
         self._major = major
         self._diemtb = diemtb
-        self._hocluc = ""
+        self._xeploai = ""
